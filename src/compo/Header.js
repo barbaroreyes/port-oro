b9ddb3f95bfec752a0804da77f9f652885499ab7
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-12 14:07:11 -0500

    upfate

--- src/compo/Header.js
+++ src/compo/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+
 const Header = () => {
   return (
     <header>
-    <h4>Barbaro Reyes </h4>
+    <h4>Barbaro Reyes</h4>
       
     </header>
   )
