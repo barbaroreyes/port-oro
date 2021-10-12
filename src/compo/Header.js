11619586c1b470a24f91e0f168fa5c35b84bf966
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-12 11:18:28 -0500

    added-routes

--- src/compo/Header.js
+++ src/compo/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const Header = () => {
   return (
     <header>
-        header
+        <ul>
+          <Link to='/'>Home</Link>
+          <Link to='/projects'>Projects</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/contact'>Contact</Link>
+        </ul>
       
     </header>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,17 +2,30 @@ import React from 'react'
 import Main from '../compo/Main'
 import Button from '../compo/Button'
 import Header from '../compo/Header'
-
+import {Switch,Route} from 'react-router-dom'
 const Home = (props) => {
 return (
     <div className="main">
-     
-     <Button 
+     <Switch>
+       <Route exact path='/'>
+         home
+       </Route>
+       <Route exact path='/projects'>
+       <Button 
      {...props}
      />
     <Main 
     {...props}
     />
+       </Route>
+       <Route exact path='/about'>
+         about
+       </Route>
+       <Route exact path='/contact'>
+         contact
+       </Route>
+     </Switch>
+    
 
     </div>
   )
